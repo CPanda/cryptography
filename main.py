8c302561f5f848e4dd52873c4fe1f1a903c06f5b
CPanda <djh33454@huskies.bloomu.edu>
2018-04-08 12:08:54 -0400

    simple main gui for the python app.

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 """
 This is the main python file that will use functions from playfair, vigenere, etc.
 
+Simple GTK Glade GUI.
+
 """
 
 import playfair
 import vigenere
+#import gtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+builder = Gtk.Builder()
+builder.add_from_file("gui.glade")
+
+window = builder.get_object("mainwin")
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+
+Gtk.main()
