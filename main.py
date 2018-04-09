340174c5648f7a083bc63334c6f7dd3fbaf9f7c6
CPanda <djh33454@huskies.bloomu.edu>
2018-04-08 20:35:00 -0400

    added some definitions for the gui

--- main.py
+++ main.py
@@ -6,17 +6,30 @@ Simple GTK Glade GUI.
 """
 
 import playfair
-import vigenere
-#import gtk
+import vigenere as vc
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+
+class Handler:
+    def decrypt_clicked(self, button):
+        print("Clciiked")
+    def encrypt_clicked_cb(self, button):
+        text = builder.get_object("maintext")
+        print(vc.encrypt(text.get_text(), 'kool'))
+        print('pressed')
+
 builder = Gtk.Builder()
 builder.add_from_file("gui.glade")
+builder.connect_signals(Handler())
 
 window = builder.get_object("mainwin")
 window.connect("destroy", Gtk.main_quit)
 window.show_all()
 
 Gtk.main()
+
+
+
+
