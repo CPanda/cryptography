7b4ff4669b31f33dfd436e582af249bb3b8dceaa
CPanda <djh33454@huskies.bloomu.edu>
2018-04-25 19:10:35 -0400

    fixes empty textbox problems. implements default ciphertext. fixes frequency analysis graph.

--- main.py
+++ main.py
@@ -37,29 +37,32 @@ class MainW (QMainWindow, Ui_MainWindow):
         self.save_image.clicked.connect(self.saveimage)
         self.save_text.setText("plot.png")
         self.split_text.clicked.connect(self.split)
+        self.default_cipher.clicked.connect(self.defaultciphertext)
+
+    def defaultciphertext(self):
+        text = self.default_cipher_textbox.toPlainText()
+        if text:
+            self.kasiski_test.setText(text)
+            self.friedman_text.setText(text)
+            self.freq_encrypt_text.setText(text)
+
     def split(self):
         text = self.freq_encrypt_text.toPlainText()
-        loopno = int(self.key_len.text())
-        print(loopno)
-        arry = []
-        for i in range(1,loopno+1):
-            arry.append(self.getNthLetters(i, loopno, text))
-        print("array values", arry)
-        #print("Result", self.getNthLetters(1,text,2))
-
-        dialog = QDialog(self)
-        dialog.ui = Ui_split_Dialog()
-        dialog.ui.setupUi(dialog)
-        #dialog.setAttribute(Qt.WA_DeleteOnClose)
-        #dialog.ui.connect(dialog.ui, SIGNAL("finished(int)"), lambda x:self.finished())
-        for i in range(len(arry)):
-            dialog.ui.split_result.append(arry[i]+'\n')
-        #dialog.resize(200,100)
-        dialog.setModal(False)
-        dialog.show()
-        #dialog.ui.split_result.setText()
-
-
+        if text:
+            loopno = int(self.key_len.text())
+            arry = []
+            for i in range(1,loopno+1):
+                arry.append(self.getNthLetters(i, loopno, text))
+            dialog = QDialog(self)
+            dialog.ui = Ui_split_Dialog()
+            dialog.ui.setupUi(dialog)
+            #dialog.setAttribute(Qt.WA_DeleteOnClose)
+            #dialog.ui.connect(dialog.ui, SIGNAL("finished(int)"), lambda x:self.finished())
+            for i in range(len(arry)):
+                dialog.ui.split_result.append(arry[i]+'\n')
+            #dialog.resize(200,100)
+            dialog.setModal(False)
+            dialog.show()
 
 
     def getNthLetters(self, n, keylen, msg):
@@ -76,33 +79,43 @@ class MainW (QMainWindow, Ui_MainWindow):
         self.freq_graph.axes.clear()
         text = self.freq_encrypt_text.toPlainText()
         text = vg.processtext(text)
-        #figure = pt.freq_analysis(text)
-        letter_count = Counter(text)
-        letter_count = sorted(letter_count.items())
-        labels, values = zip(*letter_count)
-        indexes = np.arange(len(labels))
-        width = 0.8
-        self.freq_graph.axes.bar(indexes, values, width, align='center')
-        self.freq_graph.axes.set_xticks(indexes )
-        self.freq_graph.axes.set_xticklabels(labels)
-        self.freq_graph.draw()
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        if text and alphabet:
+            text+=alphabet
+            #figure = pt.freq_analysis(text)
+            letter_count = Counter(text)
+            letter_count = sorted(letter_count.items())
+            labels, values = zip(*letter_count)
+            values = list(values)
+            indexes = np.arange(len(labels))
+            width = 0.8
+            for i in range(len(values)):
+                values[i]-=1 #take away the added values from alphabet.
+            self.freq_graph.axes.bar(indexes, values, width, align='center')
+            self.freq_graph.axes.set_xticks(indexes )
+            self.freq_graph.axes.set_xticklabels(labels)
+            self.freq_graph.draw()
         #self.freq_graph.figure.savefig('fig.png')
 
     def saveimage(self):
         text = self.save_text.text()
-        self.freq_graph.figure.savefig(text)
+        if text:
+            self.freq_graph.figure.savefig(text)
     def friedman(self):
         text = self.friedman_text.toPlainText()
-        self.friedman_results.setText(str(vg.friedmantest(text)))
+        if text:
+            self.friedman_results.setText(str(vg.friedmantest(text)))
     def vg_decrypt(self):
         text = self.vg_encrypted_text.toPlainText()
         key = self.vg_key.toPlainText()
-        self.vg_plaintextEdit.setText(vg.decrypt(text,key))
+        if text:
+            self.vg_plaintextEdit.setText(vg.decrypt(text,key))
     def vg_encrypt(self):
         text = self.vg_plaintextEdit.toPlainText()
         key = self.vg_key.toPlainText()
         #print(vg.encrypt(text, key))
-        self.vg_encrypted_text.setText(vg.encrypt(text,key))
+        if text and key:
+            self.vg_encrypted_text.setText(vg.encrypt(text,key))
     def showenglish_freq_dialog(self):
         dialog = QDialog(self)
         dialog.ui = Ui_english_freq_dialog()
